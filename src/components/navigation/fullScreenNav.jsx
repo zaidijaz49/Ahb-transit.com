744d582d1b339baa2581128148fb79ee8a61ac38
zaidijaz <zaidijaz49@gmail.com>
2025-11-03 22:14:51 +0500

    see

--- src/components/navigation/fullScreenNav.jsx
+++ src/components/navigation/fullScreenNav.jsx
@@ -133,32 +133,8 @@ const FullScreenNav = () => {
                   src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
                   alt=""
                 />
-                <h2 className="whitespace-nowrap font-[font2] lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase">
-                  SEE everything
-                </h2>
-                <img
-                  className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
-                  src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="moveX flex items-center">
-                <h2 className="whitespace-nowrap font-[font2] lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase">
-                  SEE everything
-                </h2>
-                <img
-                  className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
-                  src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
-                  alt=""
-                />
-                <h2 className="whitespace-nowrap font-[font2] lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase">
-                  SEE everything
-                </h2>
-                <img
-                  className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
-                  src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
-                  alt=""
-                />
+                
+                
               </div>
             </div>
           </div> </Link>
